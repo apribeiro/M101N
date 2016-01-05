60252689dfb519fdf022359dc1f0b118f7288655
André Ribeiro <andre.ribeiro@ist.utl.pt>
2016-01-05 21:03:03 +0000

    Week 1 artifacts added :books:

---
+++ week_1_introduction/homework_1_3/SystemCheck/src/M101DotNet.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace M101DotNet.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var i = 0;
+            for (var bit = 0; bit < 32; bit++)
+            {
+                i |= bit << bit;
+            }
+
+            ViewBag.Message = i.ToString();
+            return View();
+        }
+    }
+}
