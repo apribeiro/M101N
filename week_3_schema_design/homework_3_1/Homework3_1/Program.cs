ad7fddbc3c16828c1112734ea18e289ff91720da
André Ribeiro <andre.ribeiro@ist.utl.pt>
2016-01-23 15:33:52 +0000

    Added homework 3.1 solution :boom:

---
+++ week_3_schema_design/homework_3_1/Homework3_1/Program.cs
@@ -0,0 +1,48 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+            Console.WriteLine();
+            Console.WriteLine("Press Enter");
+            Console.ReadLine();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            string connectionString = "mongodb://localhost:27017";
+            var client = new MongoClient(connectionString);
+
+            var db = client.GetDatabase("school");
+            var students = db.GetCollection<BsonDocument>("students");
+            var list = await students.Find(new BsonDocument()).ToListAsync();
+            Dictionary<int, BsonValue> dic = new Dictionary<int, BsonValue>();
+            
+            // Get the lowest homework score by student
+            foreach (var doc in list)
+            {
+                BsonArray scores = doc["scores"].AsBsonArray;
+                var hw = scores.Where(s => s["type"] == "homework").OrderBy(s => s["score"]).First();
+                dic.Add(doc["_id"].ToInt32(), hw["score"]);
+            }
+
+            // Delete the lowest homework score of each student
+            foreach (int key in dic.Keys)
+            {
+                var filter = Builders<BsonDocument>.Filter;
+                var update = Builders<BsonDocument>.Update;
+                await students.FindOneAndUpdateAsync(filter.Eq("_id", key), update.PullFilter("scores", filter.Eq("score", dic[key])));
+            }
+        }
+    }
+}
